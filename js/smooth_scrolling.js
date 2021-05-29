df07d7ff7cfe7008b013f1f2f33b2ba9973e5dcd
Chebanro2807 <Chebanro2807@ukr.net>
2021-05-29 15:37:26 +0300

    fix some issue with smooth scrolling

--- js/smooth_scrolling.js
+++ js/smooth_scrolling.js
@@ -3,11 +3,17 @@ function createScrollAnimation() {
     for (let anchor of anchors) {
         anchor.addEventListener("click", function(e) {
             e.preventDefault();
-            let goto = document.querySelector("section[id=" + anchor.getAttribute("href").slice(1) +"]");
-            goto.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
+            console.log(window.innerWidth)
+            if (window.innerWidth < 1024) {
+                var topOfElement = document.querySelector("section[id=" + anchor.getAttribute("href").slice(1) +"]").offsetTop - 110;
+                window.scroll({ top: topOfElement, behavior: "smooth" });
+            } else {
+                let goto = document.querySelector("section[id=" + anchor.getAttribute("href").slice(1) +"]");
+                    goto.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                });
+            }
         });
     }
 }
